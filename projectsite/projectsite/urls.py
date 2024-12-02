d6cb14ee758424980af3a7c533cde3d8819d262c
Siradz Sahiddin <141974098+iaa-ai@users.noreply.github.com>
2024-12-02 14:48:09 +0800

    multiplelinechart

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -4,12 +4,13 @@ from django.urls import path
 from fire.views import HomePageView
 from fire import views
 
-from fire.views import HomePageView, ChartView, PieCountbySeverity
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
     path('chart/', PieCountbySeverity, name='chart'), 
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'), 
 
 ]
