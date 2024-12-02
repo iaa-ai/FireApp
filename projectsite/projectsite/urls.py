53ddb4c56d065f3aa9452c634ea307a7b94b6700
Siradz Sahiddin <141974098+iaa-ai@users.noreply.github.com>
2024-12-02 14:56:46 +0800

    dynamic multipleBarAchart

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from fire.views import HomePageView
 from fire import views
 
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country 
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
