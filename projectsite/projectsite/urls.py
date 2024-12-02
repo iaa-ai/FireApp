f3710c8101308f95160d15b0bcfc743f16b2129e
Siradz Sahiddin <141974098+iaa-ai@users.noreply.github.com>
2024-12-02 14:29:03 +0800

    my piechart

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -4,8 +4,12 @@ from django.urls import path
 from fire.views import HomePageView
 from fire import views
 
+from fire.views import HomePageView, ChartView, PieCountbySeverity
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'), 
 
 ]
